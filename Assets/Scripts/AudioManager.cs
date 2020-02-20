fd945d22963d1f13abbf1d4e080dc4024798117d
ilpeppino <ilpeppino@gmail.com>
2020-02-20 12:24:10 +0100

    84 - created singletons for audio and level manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class AudioManager : MonoBehaviour
+{
+
+    #region Singleton 
+
+    // Instance variable of the class
+    private static AudioManager _audioManager;
+
+    // Constructor 
+    public static AudioManager Instance
+    {
+        get
+        {
+            // If there is no instance of Audio Manager, then it creates one 
+            if (_audioManager == null)
+            {
+
+                Debug.Log("New AudioManager instance being created");
+                _audioManager = new GameObject().AddComponent<AudioManager>();
+                
+            }
+
+            return _audioManager;
+        }
+    }
+
+    private void Awake()
+    {
+
+        // Destroy the "new" instance if one is already present
+        if (_audioManager != null)
+        {
+            Debug.Log("Destroying new AudioManager");
+            Destroy(this);
+        }
+
+        Debug.Log("This: " + this);
+        DontDestroyOnLoad(this);
+    }
+
+    #endregion
+
+
+    private void Start()
+    {
+        
+        // Singleton for Audio Manager
+
+        if (this != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+
+    }
+
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    #region Local variables
+
+    private float _sceneTransitionTime = 5f;
+    private int _sceneIndex;
+
+    #endregion
+
+    #region Singleton construction
+
+    private static LevelManager _levelManager;
+
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (_levelManager == null)
+            {
+                _levelManager = new GameObject().AddComponent<LevelManager>();
+            }
+
+            return _levelManager;
+        }
+    }
+
+    private void Awake()
+    {
+        
+        if (_levelManager != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+
+    }
+
+    #endregion
+
+    void Start()
+    {
+
+        // Initializes index on loaded scene
+        _sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("Scene index: " + _sceneIndex);
+
+        // Only for splash screen, next scene is loaded after a delay
+        if (_sceneIndex == 0 )
+        {
+            Invoke("LoadNextScene", _sceneTransitionTime);
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        // Check if the following scene exists
+        if(SceneManager.GetSceneByBuildIndex(_sceneIndex + 1) == null)
+        {
+            Debug.Log("Scene loading error with build index " + _sceneIndex + 1);
+            return;
+        }
+
+        // Loads the following the scene
+        _sceneIndex++;
+        SceneManager.LoadScene(_sceneIndex);
+        Debug.Log("Scene index: " + _sceneIndex);
+        
+    }
+
+
+
+}
